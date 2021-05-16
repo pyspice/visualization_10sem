ccbe356f2116eee2f74d215e1b2c2ded38660fa6
Sergey Vinokurov <vins@1c.ru>
2021-05-16 21:18:50 +0300

    Fix footer link positioning

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,12 @@ function App() {
       </Row>
       <Row className="footer bg-secondary mw-100">
         <Col className="position-relative mw-100">
-          <a className="text-light position-absolute r-0" href="https://github.com/pyspice/visualization_10sem">Github repo</a>
+          <a
+            className="text-light position-absolute end-0"
+            href="https://github.com/pyspice/visualization_10sem"
+          >
+            Github repo
+          </a>
         </Col>
       </Row>
     </Container>
